6d03aa1f07de9432f6682ccbe53945e046b999ef
Sai Sree Harsha <sreesai1412@gmail.com>
2021-05-17 13:44:43 +0530

    Update __init__.py

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,5 +1,7 @@
 from .blender import BlenderDataset
+from .blender_online import BlenderDatasetOnline
 from .llff import LLFFDataset
 
 dataset_dict = {'blender': BlenderDataset,
+                'blender_online': BlenderDatasetOnline,
                 'llff': LLFFDataset}
